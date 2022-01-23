999a87158ede9bb7da287b99633e59a31e18fce0
Eugene Oh <73729451+Eugene-Oh@users.noreply.github.com>
2022-01-22 17:42:06 -0800

    Add files via upload
    Changed the sprite to the commando player and added movement mechanics.

--- main.js
+++ main.js
@@ -2,13 +2,27 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("./tohrusprite/tohruwalk.png")
-ASSET_MANAGER.queueDownload("./tohrusprite/tohruwalkreverse.png")
-ASSET_MANAGER.queueDownload("./tohrusprite/tohrujump.png")
-ASSET_MANAGER.queueDownload("./tohrusprite/tohruidle.png")
-ASSET_MANAGER.queueDownload("./tohrusprite/tohruuppercut.png")
-ASSET_MANAGER.queueDownload("./tohrusprite/tohrukick.png")
-ASSET_MANAGER.queueDownload("./tohrusprite/tohrufaint.png")
+ASSET_MANAGER.queueDownload("./playersprite/run.png")
+ASSET_MANAGER.queueDownload("./playersprite/runreverse.png")
+ASSET_MANAGER.queueDownload("./playersprite/runshooting.png")
+ASSET_MANAGER.queueDownload("./playersprite/runshootingreverse.png")
+
+ASSET_MANAGER.queueDownload("./playersprite/jump.png")
+ASSET_MANAGER.queueDownload("./playersprite/jumpreverse.png")
+ASSET_MANAGER.queueDownload("./playersprite/jumpshooting.png")
+ASSET_MANAGER.queueDownload("./playersprite/jumpshootingreverse.png")
+
+ASSET_MANAGER.queueDownload("./playersprite/crouched.png")
+ASSET_MANAGER.queueDownload("./playersprite/crouchedreverse.png")
+ASSET_MANAGER.queueDownload("./playersprite/crouchedshooting.png")
+ASSET_MANAGER.queueDownload("./playersprite/crouchedshootingreverse.png")
+
+
+ASSET_MANAGER.queueDownload("./playersprite/idle.png")
+ASSET_MANAGER.queueDownload("./playersprite/idlereverse.png")
+ASSET_MANAGER.queueDownload("./playersprite/idleshoot.png")
+ASSET_MANAGER.queueDownload("./playersprite/idleshootreverse.png")
+
 ASSET_MANAGER.queueDownload("./backgrounds/subway_BG.png")
 
 ASSET_MANAGER.downloadAll(() => {
@@ -16,7 +30,7 @@ ASSET_MANAGER.downloadAll(() => {
 	const ctx = canvas.getContext("2d");
 	ctx.imageSmoothingEnabled = false;
 
-	gameEngine.addEntity(new Tohru(gameEngine));
+	gameEngine.addEntity(new Player(gameEngine));
 	gameEngine.addEntity(new Background(gameEngine));
 
 
