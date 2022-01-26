bbd8fc92cc084d375dbfcad3334706470eecdd0e
Eugene Oh <73729451+Eugene-Oh@users.noreply.github.com>
2022-01-25 19:46:10 -0800

    Add files via upload
    Added jumping mechanic

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 class Background {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./backgrounds/subway_BG.png"), 
-        40, 40, 1984, 1088, 1, 1);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./backgrounds/harbor.png"), 
+        0, 0, 1984, 1088, 1, 1);
     };
 
     update() {
@@ -10,6 +10,6 @@ class Background {
     };
 
     draw(ctx) {
-        this.animator.drawFrame(this.game.clockTick, ctx, 0, 0, 3);
+        this.animator.drawFrame(this.game.clockTick, ctx, 0, 0, .665);
     };
 }
--- main.js
+++ main.js
@@ -2,23 +2,42 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("./tohrusprite/tohruwalk.png")
-ASSET_MANAGER.queueDownload("./tohrusprite/tohruwalkreverse.png")
-ASSET_MANAGER.queueDownload("./tohrusprite/tohrujump.png")
-ASSET_MANAGER.queueDownload("./tohrusprite/tohruidle.png")
-ASSET_MANAGER.queueDownload("./tohrusprite/tohruuppercut.png")
-ASSET_MANAGER.queueDownload("./tohrusprite/tohrukick.png")
-ASSET_MANAGER.queueDownload("./tohrusprite/tohrufaint.png")
-ASSET_MANAGER.queueDownload("./backgrounds/subway_BG.png")
+ASSET_MANAGER.queueDownload("./playersprite/run.png")
+ASSET_MANAGER.queueDownload("./playersprite/runreverse.png")
+ASSET_MANAGER.queueDownload("./playersprite/runshooting.png")
+ASSET_MANAGER.queueDownload("./playersprite/runshootingreverse.png")
+ASSET_MANAGER.queueDownload("./playersprite/jump.png")
+ASSET_MANAGER.queueDownload("./playersprite/jumpreverse.png")
+ASSET_MANAGER.queueDownload("./playersprite/jumpshooting.png")
+ASSET_MANAGER.queueDownload("./playersprite/jumpshootingreverse.png")
+ASSET_MANAGER.queueDownload("./playersprite/crouched.png")
+ASSET_MANAGER.queueDownload("./playersprite/crouchedreverse.png")
+ASSET_MANAGER.queueDownload("./playersprite/crouchedshooting.png")
+ASSET_MANAGER.queueDownload("./playersprite/crouchedshootingreverse.png")
+ASSET_MANAGER.queueDownload("./playersprite/idle.png")
+ASSET_MANAGER.queueDownload("./playersprite/idlereverse.png")
+ASSET_MANAGER.queueDownload("./playersprite/idleshoot.png")
+ASSET_MANAGER.queueDownload("./playersprite/idleshootreverse.png")
+
+ASSET_MANAGER.queueDownload("./snipersprite/snipershooting.png")
+ASSET_MANAGER.queueDownload("./snipersprite/snipershootingreverse.png")
+
+ASSET_MANAGER.queueDownload("./Harbor_Assets.png")
+
+ASSET_MANAGER.queueDownload("./backgrounds/harbor.png")
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 	ctx.imageSmoothingEnabled = false;
-
-	gameEngine.addEntity(new Tohru(gameEngine));
+	gameEngine.addEntity(new Player(gameEngine));
+	gameEngine.addEntity(new Sniper(gameEngine, 980, 383, 0));
+	gameEngine.addEntity(new Sniper(gameEngine, 375, 0, 1));
+	gameEngine.addEntity(new box(gameEngine,705,1,511,297,128,127));
 	gameEngine.addEntity(new Background(gameEngine));
 
+	//	gameEngine.addEntity(new boundingfloor(gameEngine));
+	//	gameEngine.addEntity(new box(gameEngine,100,150,832,896,128,128));
 
 	gameEngine.init(ctx);
 
