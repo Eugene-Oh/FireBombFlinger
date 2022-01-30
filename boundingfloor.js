2a2de1204d7e9d237c472d4456d6a5bc9aa1c64c
Eugene Oh <73729451+Eugene-Oh@users.noreply.github.com>
2022-01-29 18:35:24 -0800

    Add files via upload

--- boundingfloor.js
+++ boundingfloor.js
@@ -1,6 +1,6 @@
 class boundingfloor { 
     constructor(game,x,y,width,height) { 
-        Object.assign(this, { x, y, width, height }); 
+        Object.assign(this, { game, x, y, width, height }); 
         this.BB = new BoundingBox(this.x,this.y,this.width,this.height); 
        // this.BB = new BoundingBox(0,510,1984,10); 
     };
